3675489e4b1847de93ce369934f48cdcfb8efa98
Pedro Moura <phvm@cin.ufpe.br>
2022-09-12 17:06:53 -0300

    feat: half-done login screen

---
+++ Projeto Entrega 2/frontend/src/routes.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from './pages/LoginScreen'
+import { Links } from './types/enums'
+
+function AppRoutes(){
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path={Links.Login} element={LoginScreen} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default AppRoutes;
---
+++ Projeto Entrega 2/frontend/src/types/enums.ts
@@ -0,0 +1,5 @@
+export enum Links {
+    Login= '/',
+    Cadastro= '/register',
+    Home= '/home'
+}
