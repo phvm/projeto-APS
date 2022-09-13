2f04ebccd2db54c3be7f381b9d89d3c80ce96e47
Pedro Moura <phvm@cin.ufpe.br>
2022-09-13 16:12:36 -0300

    feat: basic structure for registerscreen and registerforms

---
+++ Projeto Entrega 2/frontend/src/pages/RegisterScreen/index.tsx
@@ -0,0 +1,21 @@
+import { Box, Typography } from '@mui/material';
+import registerImg from '../../assets/registerImg.svg';
+import RegisterForm from '../../Components/RegisterForm';
+
+const RegisterScreen = () => {
+    return(
+        <Box sx={{width: '100vw', height: '100vh', backgroundColor: '#05D5B2', display: 'flex'}}>
+            <Box sx={{width: '60%', height: '80%', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', marginTop:'5.5em'}}>
+                <Typography align='center' variant='h1' sx={{color: '#f2f0f0'}}>
+                    vaCInnação
+                </Typography>
+                <img src={registerImg} alt="" />
+            </Box>
+            <Box sx={{width: '40%', height: '100%', display: 'flex', justifyContent: 'left', alignItems: 'center'}}>
+                <RegisterForm />
+            </Box>
+        </Box>
+    );
+};
+
+export default RegisterScreen;
