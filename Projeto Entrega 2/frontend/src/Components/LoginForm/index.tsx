4fc6da6661f75c806c00817606caeff3467d323a
Pedro Moura <phvm@cin.ufpe.br>
2022-09-16 16:15:19 -0300

    feat: login form

--- Projeto Entrega 2/frontend/src/Components/LoginForm/index.tsx
+++ Projeto Entrega 2/frontend/src/Components/LoginForm/index.tsx
@@ -1,30 +1,32 @@
-import { Box, Typography, FormControl, OutlinedInput, Input, InputLabel, Button } from "@mui/material";
+import { Box, Typography, FormControl, OutlinedInput, InputLabel, Button } from "@mui/material";
 import { LoginFormProps } from '../../types/componentsTypes/LoginFormProps';
 
 const LoginForm = (props: LoginFormProps) => {
     return(
         <Box sx={{boxSizing: 'border-box', width: '80%', height: '80%', backgroundColor: '#F2F0F0', borderRadius: '35px', padding: '3em', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly'}}>
-            <Typography variant="h3" align='left' sx={{color: '#E63462', fontSize: '2.5rem'}}>
-                Bem-vindo de volta!
-            </Typography>
-            <Typography variant="h5" align='left' sx={{color: '#262626', fontSize: '1.5rem'}}>
-                Por favor, informe seu CPF e senha
-            </Typography>
-            <Box sx={{ width: '100%', display: 'flex', flexDirection: 'column', }}>
-                <FormControl>
+            <Box sx={{display: 'flex', flexDirection: 'column', height: '15%', justifyContent: 'space-around'}}>
+                <Typography variant="h3" align='left' sx={{color: '#E63462', fontSize: '2.5rem'}}>
+                    Bem-vindo de volta!
+                </Typography>
+                <Typography variant="h5" align='left' sx={{color: '#262626', fontSize: '1.5rem'}}>
+                    Por favor, informe seu CPF e senha.
+                </Typography>
+            </Box>
+            <Box sx={{ width: '100%', height: '40%', display: 'flex', flexDirection: 'column', justifyContent: 'space-around', alignItems: 'center'}}>
+                <FormControl sx={{width: '100%'}}>
                     <InputLabel htmlFor="component-outlined">CPF</InputLabel>
                     <OutlinedInput required placeholder="Digite seu CPF..." />
                 </FormControl>
-                <FormControl>
+                <FormControl sx={{width: '100%'}}>
                     <InputLabel>Senha</InputLabel>
                     <OutlinedInput required placeholder="Digite sua senha..." />
                 </FormControl>
-                <Button>Login</Button>
+                <Button sx={{color: '#E63462', fontSize: '1rem', fontWeight: '600', width: '20%'}}>Login</Button>
             </Box>
             <Box>
                 <Typography variant="h5" align='center'>
                     Se ainda não é cadastrado <br/>
-                    <p style={{color: '#E63462'}}>clique aqui!</p>
+                    <a href="" style={{color: '#E63462'}}>clique aqui!</a>
                 </Typography>
             </Box>
         </Box>
